5791ca7221f96a161770ac5586382bf99d006b25
Paulo Junior <paulojpjunior@gmail.com>
2024-07-25 20:21:09 -0300

    configuração de texto
    *criação dos botões da barra de ferramentas
    *fazendo a transformação do texto em html

--- text_config/src/App.jsx
+++ text_config/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState, useEffect, useRef } from "react";
+import { marked } from "marked";
+
 import BarraFerramentas from "./components/BarraFerramentas";
 
 function App() {
+  const [text, setText] = useState("###Hello word!!!");
+
+  //função responsável por transformar em html.
+  const renderText = () => {
+    return { __html: marked(text) };
+  };
+
+  const textAreaRef = useRef(null);
+
   return (
     <div className="app-container">
       <BarraFerramentas />
-      <textarea></textarea>
+      <textarea
+        ref={textAreaRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <div dangerouslySetInnerHTML={renderText()} />
     </div>
   );
 }
--- text_config/src/components/BarraFerramentas.jsx
+++ text_config/src/components/BarraFerramentas.jsx
@@ -2,7 +2,16 @@
 import React from "react";
 
 const BarraFerramentas = () => {
-  return <div>Barra de Ferramentas</div>;
+  return <div className="toolbar">
+    <button>h1</button>
+    <button>h2</button>
+    <button>Bold</button>
+    <button>Italico</button>
+    <button>Link</button>
+    <button>Code Block</button>
+    <button>List Item</button>
+    <button>Horizontal Line</button>
+  </div>;
 };
 
 export default BarraFerramentas;
